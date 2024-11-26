b599f457fd6755fde59464d62b36732e1a534199
Marcus Sandes <marcus.sandesf@gmail.com>
2024-11-26 11:27:35 -0300

    feat: implementation update

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Client;
 
 public class Program {
 
@@ -9,58 +12,49 @@ public class Program {
 		
 		Locale.setDefault(Locale. US);
 		Scanner sc = new Scanner(System.in);
+		Client account;
+		
+		
 		
 		// Numero da conta
 		System.out.print("Enter account number: ");
-		int accountNumber = sc.nextInt();
+		int number = sc.nextInt();
 		sc.nextLine();
 		
 		// Nome do titular da conta
 		System.out.print("Enter account holder: ");
-		String accountHolder = sc.nextLine();
+		String holder = sc.nextLine();
 		
 		
 		// Deseja fazer o deposito inicial
 		System.out.print("Is there na initial deposit (Y/N)? ");
-		char initialDeposit = sc.next().toUpperCase().charAt(0);	
-		
-		double depositValue = 0.0;
-			if (initialDeposit == 'Y') {
+		char response = sc.next().toUpperCase().charAt(0);	
+			if (response == 'Y') {
 				System.out.print("Enter initial deposit value: ");
-				depositValue = sc.nextDouble();
+				double initialDeposit = sc.nextDouble();
+				account = new Client(number, holder, initialDeposit);
 		}
-		
+			else {
+				account = new Client(number, holder);
+			}
+			
 		System.out.println();
 		System.out.println("Account data: ");
-		System.out.println("Account: " + accountNumber);
-		System.out.println("Holder: " + accountHolder);
-		System.out.println("Balance: $" + depositValue);
+		System.out.println(account);
+		
 		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		double depositValue = sc.nextDouble();
+		account.deposit(depositValue);
+		System.out.println("Update account data:");
+		System.out.println(account);
 		
-		System.out.print("Enter deposit value: ");
-		double depositValue1 = sc.nextDouble();
-		double totalDeposit = depositValue + depositValue1;
-		
-		System.out.println("Update account data: ");
-		System.out.println("Account: " + accountNumber);
-		System.out.println("Holder: " + accountHolder);
-		System.out.println("Balance: $" + totalDeposit);
 		System.out.println();
-		
-		System.out.print("Enter withdraw value: ");
-		double withdrawValue = sc.nextDouble(); 
-		double balance = (totalDeposit - withdrawValue) - 5;
-		System.out.println("Update account data: ");
-		System.out.println("Account: " + accountNumber);
-		System.out.println("Holder: " + accountHolder);
-		System.out.println("Balance: $" + balance);
-		
-		
-		
-		
-		
-		
-		
+		System.out.print("Enter a withdraw value: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		System.out.println("Update account data:");
+		System.out.println(account);
 		
 		sc.close();
 	}
